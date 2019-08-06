65cf27b8800fd3fbd4163afe9b1860c06a2741e7
Brian S Evans <bsevans.unc@gmail.com>
2019-08-05 22:29:34 -0400

    initial commit

---
+++ scripts/tidyverse_exercise.R
@@ -0,0 +1,77 @@
+# setup -------------------------------------------------------------------
+
+# Load libraries:
+
+library(tidyverse)
+library(lubridate)
+
+# setup -------------------------------------------------------------------
+
+gitUrl <-
+  'https://raw.githubusercontent.com/bsevansunc/'
+
+courseData <-
+  'smsc_data_science/master/data/'
+
+measurements <- 
+  read_csv(
+    paste0(
+      gitUrl,
+      courseData,
+      'measurements.csv'))
+
+origins <- 
+  read_csv(
+    paste0(
+      gitUrl,
+      courseData,
+      'origins.csv'))
+
+# 1. The population of the Bahamas -------------------------------------------
+
+population
+
+# Calculate the population of the Bahamas from 1995-2013. Return your results as 
+# a two-column data frame with the columns year and population.
+
+
+# 2. States ---------------------------------------------------------------
+
+states1975
+
+# Use group_by and summarize to calculate the population density of each region.
+# Return your results as a two-column data frame with the columns region and 
+# popDensity.    
+
+
+# 3. Counting birds at site "apple" ---------------------------------------
+
+birdCounts
+
+birdHabits
+
+# Calculate the total number of each diet class observed at site "apple" in  2009.
+# Return your results as a two-column data frame with the columns diet and count.
+
+
+# 4. Star Wars ------------------------------------------------------------
+
+measurements
+
+origins
+
+# Calculate the average mass and number of characters associated with each  
+# species of the Star Wars universe. Return your results as a three-column data
+# frame with the columns: species, nCharacters, and meanMass.
+
+
+# Iris petal shape --------------------------------------------------------
+
+irisTbl
+
+# You have developed a derived variable, "petalShape" to describe Iris flowers. 
+# This variable is defined as the ratio of petalLength to petalWidth 
+# (petalLength/petalWidth). Calculate the average (mean) shape of petals in the
+# iris dataset. Return your results as a two-column data frame with the columns 
+# species and petalShape_mean.
+
